4efe51ff39a65ad466138abd2784f689a3ab1ea4
Lộc Nguyễn <loc16910@gmail.com>
2021-02-23 13:18:55 +0700

    cmt 1st

---
+++ Hieu.py
@@ -0,0 +1,9 @@
+you = "a"
+
+if you == "":
+    robot_brain = "i can't hear you, try again"
+elif you == "hello":
+    robot_brain = "Hi"
+else:
+    robot_brain = "I'm fine thanks u and u"
+print(robot_brain)
---
+++ Noi.py
@@ -0,0 +1,6 @@
+import pyttsx3
+robot_brain = "Hello"
+# nhập để nới
+robot_mouth = pyttsx3.init()
+robot_mouth.say("I can't hear you, try again")
+robot_mouth.runAndWait()
